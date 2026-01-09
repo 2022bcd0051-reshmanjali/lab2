db420fcc2ef5bef599bd590669c9765578eea275
Anjali Maddula <anjalimaddula@Anjalis-MacBook-Air.local>
2026-01-09 15:59:59 +0530

    Model-Ridge alpha-1.0 test-split-0.2 scaler

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ import joblib
 import os
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import Lasso
+from sklearn.linear_model import Ridge
 from sklearn.metrics import mean_squared_error, r2_score
 
 os.makedirs("output", exist_ok=True)
@@ -25,8 +25,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 # Model
-from sklearn.linear_model import Lasso
-model = Lasso(alpha=0.1)
+model = Ridge(alpha=1.0)
 model.fit(X_train, y_train)
 
 # Predict
