d152eac3233e87972ad29a4abb487e7879575539
Anjali Maddula <anjalimaddula@Anjalis-MacBook-Air.local>
2026-01-09 16:04:28 +0530

    Model-RandomForest n_estimators-100 max_depth-10

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ import joblib
 import os
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
 
 os.makedirs("output", exist_ok=True)
@@ -25,9 +25,11 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 # Model
-model = DecisionTreeRegressor(
-    max_depth=5,
-    random_state=42
+model = RandomForestRegressor(
+    n_estimators=100,
+    max_depth=10,
+    random_state=42,
+    n_jobs=-1
 )
 model.fit(X_train, y_train)
 
